0d86bc4dee86945d1f7bdacfdbc0890cb8d58f5b
Pratik Kulkarni <pnkulkarni@proton.me>
2026-04-04 04:02:29 +0000

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,6 +1,14 @@
 const net = require("net");
 
 const server = net.createServer((connection) => {
-    console.log("do somethign");
+  // console.log("do somethign on connection recvd");
+
+  // connection.on('data', () => {
+  //   console.log(connection.bytesRead)
+  // })
+
+  connection.write('+PONG\r\n')
 });
+
 server.listen(6379, "127.0.0.1");
+// console.log("listening on port 6379")
